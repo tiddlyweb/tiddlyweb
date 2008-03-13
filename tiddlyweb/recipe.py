07b2608c0863290f1f20524cdd416b0c6038b9ea
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-13 17:50:48 +0000

    Remove an unused method (it had been moved to control earlier).
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@3944 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- tiddlyweb/recipe.py
+++ tiddlyweb/recipe.py
@@ -37,26 +37,3 @@ class Recipe(list):
         """
         return self
 
-    def determine_bag(self, tiddler):
-        """
-        Return the bag which this tiddler would be in if we 
-        were to save it to the recipe rather than to a default
-        bag.
-
-        This is a matter of reversing the recipe list and seeing
-        if the tiddler is a part of the bag + filter. If bag+filter
-        is true, return that bag.
-        """
-        for bag, filter_string in reversed(self):
-            if type(bag) == str:
-                bag = Bag(name=bag)
-            # add our tiddler to the bag to see if it survives
-            # the filter
-            bag.add_tiddler(tiddler)
-            for candidate_tiddler in filter.filter_bag(bag, filter_string):
-                if tiddler.title == candidate_tiddler.title:
-                    return bag
-
-        return None
-
-    
