34b15759f9110920329a2a58d50d6d8f57430f18
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-13 01:33:25 +0000

    move the muchdata data created into fixtures so we can use it elsewhere
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@3926 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- test/fixtures.py
+++ test/fixtures.py
@@ -10,6 +10,7 @@ import shutil
 
 from tiddlyweb.bag import Bag
 from tiddlyweb.tiddler import Tiddler
+from tiddlyweb.recipe import Recipe
 
 tiddlers = [
         Tiddler(
@@ -64,4 +65,44 @@ def reset_textstore():
     shutil.rmtree(textstore.store_dirname)
     os.makedirs(textstore.bag_store)
     os.makedirs(textstore.recipe_store)
+
+def muchdata(store):
+    for bag_numeral in range(30):
+        bag = create_bag(store, bag_numeral)
+        for tiddler_numeral in range(10):
+            tiddler = create_tiddler(store, bag, tiddler_numeral)
+
+    recipe = Recipe('long')
+
+    recipe_list = []
+    for numeral in range(0, 30, 2):
+        bag_name = 'bag%s' % numeral
+        filter_string = 'tiddler%s' % (numeral % 10)
+        if not (numeral % 10) % 3:
+            filter_string = filter_string + ' [tag[tag three]]'
+        recipe_list.append([bag_name, filter_string])
+    recipe.set_recipe(recipe_list)
+
+    store.put(recipe)
+
+
+def create_tiddler(store, bag, numeral):
+    tiddler = Tiddler('tiddler%s' % numeral)
+    tiddler.bag = bag.name
+    tiddler.content = 'i am tiddler %s' % numeral
+    tags = ['basic tag']
+    if not numeral % 2:
+        tags.append('tagtwo')
+    if not numeral % 3:
+        tags.append('tagthree')
+    if not numeral % 4:
+        tags.append('tagfour')
+    tiddler.tags = tags
+    store.put(tiddler)
+
+def create_bag(store, numeral):
+    bag = Bag('bag%s' % numeral)
+    store.put(bag)
+    return bag
+
 
--- test/test_muchdata.py
+++ test/test_muchdata.py
@@ -15,42 +15,25 @@ from tiddlyweb.recipe import Recipe
 from tiddlyweb import control
 from tiddlyweb import filter
 
-from fixtures import reset_textstore
+from fixtures import reset_textstore, muchdata
 
 def setup_module(module):
     reset_textstore()
     module.store = Store('text')
+    muchdata(module.store)
 
 def test_many_bags_and_tiddlers():
     """
     Create a bunch of bags and tiddlers.
     """
 
-    for bag_numeral in range(30):
-        bag = create_bag(bag_numeral)
-        for tiddler_numeral in range(10):
-            tiddler = create_tiddler(bag, tiddler_numeral)
-
     assert len(os.listdir('store/bags')) == 30, '30 bags created'
     assert len(os.listdir('store/bags/bag0/tiddlers')) == 10, '10 tiddlers created in a bag'
 
 def test_long_recipe():
     """
-    Store a long recipe.
+    Check muchdata() stored a recipe
     """
-
-    recipe = Recipe('long')
-
-    recipe_list = []
-    for numeral in range(0, 30, 2):
-        bag_name = 'bag%s' % numeral
-        filter_string = 'tiddler%s' % (numeral % 10)
-        if not (numeral % 10) % 3:
-            filter_string = filter_string + ' [tag[tag three]]'
-        recipe_list.append([bag_name, filter_string])
-    recipe.set_recipe(recipe_list)
-
-    store.put(recipe)
 
     assert os.path.exists('store/recipes/long'), 'long recipe put to disk'
 
@@ -93,21 +76,3 @@ def test_filter_tiddlers_from_bag():
     tiddlers = control.filter_tiddlers_from_bag(bag, '[tag[tagfour]]')
     assert len(tiddlers) == 3, 'there are 3 tiddlers when filters on tagfour'
 
-def create_tiddler(bag, numeral):
-    tiddler = Tiddler('tiddler%s' % numeral)
-    tiddler.bag = bag.name
-    tiddler.content = 'i am tiddler %s' % numeral
-    tags = ['basic tag']
-    if not numeral % 2:
-        tags.append('tagtwo')
-    if not numeral % 3:
-        tags.append('tagthree')
-    if not numeral % 4:
-        tags.append('tagfour')
-    tiddler.tags = tags
-    store.put(tiddler)
-
-def create_bag(numeral):
-    bag = Bag('bag%s' % numeral)
-    store.put(bag)
-    return bag
