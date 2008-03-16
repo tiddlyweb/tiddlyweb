aab05acc631acee35d1bdbf992a87005bdeadac4
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-16 22:58:23 +0000

    remove a comment because we did what it suggested
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@3999 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- tiddlyweb/web/__init__.py
+++ tiddlyweb/web/__init__.py
@@ -1,13 +1,6 @@
 from tiddlyweb.web.http import HTTP415
 
 def get_serialize_type(environ, serializers):
-    # Use the accept headers to look up how we should serialize.
-    # If we don't do that, and we had an extension, throw a 415,
-    # otherwise, just do a default, this is needed to deal with
-    # browsers promiscuiously asking for random stuff like text/xml.
-    # It would be better if the info in tiddlyweb.accept was a 
-    # list which we traverse until a hit. Will FIXME to do that
-    # soonish.
     accept = environ.get('tiddlyweb.accept')[:]
     ext = environ.get('tiddlyweb.extension')
     serialize_type, mime_type = None, None
