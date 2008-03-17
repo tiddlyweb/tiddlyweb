226443b21d5c464072d16f9c336fa9b40820b529
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-17 12:02:05 +0000

    Adjust the server so it will create the store and put some data in it if there is none. It's a demo server after all.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@4015 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 
 import os
 from tiddlyweb.web import serve
+from tiddlyweb.store import Store
 from test import fixtures
 
 def start():
     if not os.path.exists(fixtures.textstore.store_dirname):
         fixtures.reset_textstore()
+        stores = Store('text')
+        fixtures.muchdata(stores)
     serve.start_simple('./urls.map', 8080)
 
 if __name__ == '__main__':
