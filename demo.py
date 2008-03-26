71aa188a781734c8caeaa3025da8bb3f0f9a44d2
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-03-26 05:09:50 +0000

    Update docs and create a demo.py that makes it possible to checkout the code, then do 'python demo.py' and play.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@4135 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ demo.py
@@ -0,0 +1,21 @@
+
+import os
+
+import server
+
+store_name = 'store'
+real_store = 'docstore'
+
+def go():
+    try: 
+        os.symlink(real_store, store_name)
+    except OSError:
+        print 'symlink failed, continuing'
+    try:
+        server.start('0.0.0.0', '8080')
+    except KeyboardInterrupt:
+        print 'shutting down'
+    os.unlink(store_name)
+
+if __name__ == '__main__':
+    go()
