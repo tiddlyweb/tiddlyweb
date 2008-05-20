9ed29b3387e727649b2e4e5d08bfaa05a870a68d
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-20 14:57:26 +0000

    tidlyweb - rearrange tiddlyweb.web.tiddler and in process find and fix bug (apparently I haven't got good coverage in the tests, will investigate figlear) that put in in recent refactoring. When we try to find a candidate bag for a tiddler we need to use a different algorithm when we PUT from when we GET.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@5070 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- tiddlyweb/web/tiddler.py
+++ tiddlyweb/web/tiddler.py
@@ -8,60 +8,52 @@ from tiddlyweb.web.http import HTTP404, HTTP415, HTTP409
 from tiddlyweb import control
 from tiddlyweb import web
 
-def get_revisions(environ, start_response):
-    tiddler = _determine_tiddler(environ)
-    return _send_tiddler_revisions(environ, start_response, tiddler)
-
 def get(environ, start_response):
-    tiddler = _determine_tiddler(environ)
+    tiddler = _determine_tiddler(environ, control.determine_tiddler_bag_from_recipe)
     return _send_tiddler(environ, start_response, tiddler)
 
-def _send_tiddler_revisions(environ, start_response, tiddler):
-    store = environ['tiddlyweb.store']
-
-    tmp_bag = Bag('tmp', tmpbag=True, revbag=True)
-    for revision in store.list_tiddler_revisions(tiddler):
-        tmp_tiddler = Tiddler(title=tiddler.title, revision=revision, bag=tiddler.bag)
-        try:
-            store.get(tmp_tiddler)
-        except NoTiddlerError, e:
-            raise HTTP404, 'tiddler %s at revision % not found, %s' % (tiddler.title, revision, e)
-        tmp_bag.add_tiddler(tmp_tiddler)
-
-    serialize_type, mime_type = web.get_serialize_type(environ)
-    serializer = Serializer(serialize_type)
-    serializer.object = tmp_bag
-
-    start_response("200 OK", [('Content-Type', mime_type),
-             ('Set-Cookie', 'chkHttpReadOnly=false')])
-    return [serializer.to_string()]
-
-def _send_tiddler(environ, start_response, tiddler):
-
-    store = environ['tiddlyweb.store']
-
-    try:
-        store.get(tiddler)
-    except NoTiddlerError, e:
-        raise HTTP404, '%s not found, %s' % (tiddler.title, e)
-
-    serialize_type, mime_type = web.get_serialize_type(environ)
-    serializer = Serializer(serialize_type)
-    serializer.object = tiddler
-
-    try:
-        content = serializer.to_string()
-    except TiddlerFormatError, e:
-        raise HTTP415, e
-
-    start_response("200 OK",
-            [('Content-Type', mime_type)])
-
-    return [content]
+def get_revisions(environ, start_response):
+    tiddler = _determine_tiddler(environ, control.determine_tiddler_bag_from_recipe)
+    return _send_tiddler_revisions(environ, start_response, tiddler)
 
 def put(environ, start_response):
-    tiddler = _determine_tiddler(environ)
+    tiddler = _determine_tiddler(environ, control.determine_bag_for_tiddler)
     return _put_tiddler(environ, start_response, tiddler)
+
+def _determine_tiddler(environ, bag_finder):
+    tiddler_name = environ['wsgiorg.routing_args'][1]['tiddler_name']
+    revision = environ['wsgiorg.routing_args'][1].get('revision', None)
+    if revision:
+        revision = web.handle_extension(environ, revision)
+    else:
+        tiddler_name = web.handle_extension(environ, tiddler_name)
+
+    if revision:
+        try:
+            revision = int(revision)
+        except ValueError, e:
+            raise HTTP404, '%s not a revision of %s: %s' % (revision, tiddler_name, e)
+
+    tiddler = Tiddler(tiddler_name)
+    tiddler.revision = revision
+
+    try:
+        recipe_name = environ['wsgiorg.routing_args'][1]['recipe_name']
+        recipe = Recipe(recipe_name)
+        store = environ['tiddlyweb.store']
+        store.get(recipe)
+
+        try:
+            bag = bag_finder(recipe, tiddler)
+        except NoBagError, e:
+            raise HTTP404, '%s not found, %s' % (tiddler.title, e)
+
+        bag_name = bag.name
+    except KeyError:
+        bag_name = environ['wsgiorg.routing_args'][1]['bag_name']
+
+    tiddler.bag = bag_name
+    return tiddler
 
 def _put_tiddler(environ, start_response, tiddler):
     store = environ['tiddlyweb.store']
@@ -89,38 +81,45 @@ def _put_tiddler(environ, start_response, tiddler):
 
     return []
 
-def _determine_tiddler(environ):
-    tiddler_name = environ['wsgiorg.routing_args'][1]['tiddler_name']
-    revision = environ['wsgiorg.routing_args'][1].get('revision', None)
-    if revision:
-        revision = web.handle_extension(environ, revision)
-    else:
-        tiddler_name = web.handle_extension(environ, tiddler_name)
-
-    if revision:
-        try:
-            revision = int(revision)
-        except ValueError, e:
-            raise HTTP404, '%s not a revision of %s: %s' % (revision, tiddler_name, e)
-
-    tiddler = Tiddler(tiddler_name)
-    tiddler.revision = revision
+def _send_tiddler(environ, start_response, tiddler):
+    store = environ['tiddlyweb.store']
 
     try:
-        recipe_name = environ['wsgiorg.routing_args'][1]['recipe_name']
-        recipe = Recipe(recipe_name)
-        store = environ['tiddlyweb.store']
-        store.get(recipe)
+        store.get(tiddler)
+    except NoTiddlerError, e:
+        raise HTTP404, '%s not found, %s' % (tiddler.title, e)
 
+    serialize_type, mime_type = web.get_serialize_type(environ)
+    serializer = Serializer(serialize_type)
+    serializer.object = tiddler
+
+    try:
+        content = serializer.to_string()
+    except TiddlerFormatError, e:
+        raise HTTP415, e
+
+    start_response("200 OK",
+            [('Content-Type', mime_type)])
+
+    return [content]
+
+def _send_tiddler_revisions(environ, start_response, tiddler):
+    store = environ['tiddlyweb.store']
+
+    tmp_bag = Bag('tmp', tmpbag=True, revbag=True)
+    for revision in store.list_tiddler_revisions(tiddler):
+        tmp_tiddler = Tiddler(title=tiddler.title, revision=revision, bag=tiddler.bag)
         try:
-            bag = control.determine_bag_for_tiddler(recipe, tiddler)
-        except NoBagError, e:
-            raise HTTP404, '%s not found, %s' % (tiddler.title, e)
+            store.get(tmp_tiddler)
+        except NoTiddlerError, e:
+            raise HTTP404, 'tiddler %s at revision % not found, %s' % (tiddler.title, revision, e)
+        tmp_bag.add_tiddler(tmp_tiddler)
 
-        bag_name = bag.name
-    except KeyError:
-        bag_name = environ['wsgiorg.routing_args'][1]['bag_name']
+    serialize_type, mime_type = web.get_serialize_type(environ)
+    serializer = Serializer(serialize_type)
+    serializer.object = tmp_bag
 
-    tiddler.bag = bag_name
-    return tiddler
+    start_response("200 OK", [('Content-Type', mime_type),
+             ('Set-Cookie', 'chkHttpReadOnly=false')])
+    return [serializer.to_string()]
 
