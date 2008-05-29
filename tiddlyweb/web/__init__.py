8e9a7a86f3ad926eb7b91c04dbba900d65b6a230
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-05-29 11:16:42 +0000

    tiddlyweb - python 2.4 datetime does not have strptime, so use the one from time
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@5229 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- tiddlyweb/web/__init__.py
+++ tiddlyweb/web/__init__.py
@@ -1,4 +1,5 @@
 import urllib
+import time
 from datetime import datetime
 
 from tiddlyweb.web.http import HTTP415
@@ -89,5 +90,5 @@ def bag_url(environ, bag):
     return '%s://%s/bags/%s' % (scheme, host, urllib.quote(bag.name))
 
 def http_date_from_timestamp(timestamp):
-    timestamp_datetime = datetime.strptime(timestamp, '%Y%m%d%H%M')
+    timestamp_datetime = datetime(*(time.strptime(timestamp, '%Y%m%d%H%M')[0:6]))
     return timestamp_datetime.strftime('%a, %d %b %Y %H:%M:%S GMT')
