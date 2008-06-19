69f1eecc07be6793509658c113048b649d06dc4c
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-06-19 00:24:55 +0000

    tiddlyweb - make the importer script more flexible:
    Now you can use arguments:
    
    python test/importer.py <filename to import> <hostname of server> <port of server>
    
    
    
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@5664 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- test/importer.py
+++ test/importer.py
@@ -13,7 +13,7 @@ import urllib
 def _html_decode(text):
     return text.replace('&gt;', '>').replace('&lt;', '<').replace('&amp;', '&').replace('&quot;', '"')
 
-def import_wiki(filename='wiki'):
+def import_wiki(filename='wiki', hostname='localhost', port=8080):
     f = codecs.open(filename, encoding='utf-8')
     wikitext = f.read()
     f.close()
@@ -22,27 +22,27 @@ def import_wiki(filename='wiki'):
     store_area = soup.find('div', id='storeArea')
     divs = store_area.findAll('div')
 
-    _do_recipe()
-    _do_bag()
+    _do_recipe(hostname, port)
+    _do_bag(hostname, port)
 
     for tiddler in divs:
-        _do_tiddler(tiddler)
+        _do_tiddler(hostname, port, tiddler)
 
-def _do_recipe():
+def _do_recipe(hostname, port):
     json_string = simplejson.dumps([['wiki','']])
     http = httplib2.Http()
-    url = 'http://localhost:8080/recipes/%s' % 'wiki'
+    url = 'http://%s:%s/recipes/%s' % (hostname, port, 'wiki')
     response, content = http.request(url, method='PUT', \
             headers={'Content-Type': 'application/json'}, body=json_string)
 
-def _do_bag():
+def _do_bag(hostname, port):
     json_string = simplejson.dumps({'policy': dict(), 'name': 'wiki'})
     http = httplib2.Http()
-    url = 'http://localhost:8080/bags/%s' % 'wiki'
+    url = 'http://%s:%s/bags/%s' % (hostname, port, 'wiki')
     response, content = http.request(url, method='PUT', \
             headers={'Content-Type': 'application/json'}, body=json_string)
 
-def _do_tiddler(tiddler):
+def _do_tiddler(hostname, port, tiddler):
     tiddler_dict = {}
     tiddler_dict['title'] = tiddler['title']
     tiddler_dict['text'] = _html_decode(tiddler.find('pre').contents[0])
@@ -55,11 +55,12 @@ def _do_tiddler(tiddler):
     json_string = simplejson.dumps(tiddler_dict)
 
     http = httplib2.Http()
-    url = 'http://localhost:8080/bags/wiki/tiddlers/%s' % urllib.quote(tiddler_dict['title'])
+    url = 'http://%s:%s/bags/wiki/tiddlers/%s' % (hostname, port, urllib.quote(tiddler_dict['title']))
     response, content = http.request(url, method='PUT', \
             headers={'Content-Type': 'application/json'}, body=json_string)
     if response['status'] != '204':
         print '%s: %s' % (response['status'], content)
+        sys.exit(1)
 
 def _tag_string_to_list(string):
     tags = []
@@ -73,4 +74,4 @@ def _tag_string_to_list(string):
     return tags
 
 if __name__ == '__main__':
-    import_wiki()
+    import_wiki(filename=sys.argv[1], hostname=sys.argv[2], port=sys.argv[3C)
