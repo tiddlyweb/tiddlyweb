ac2506fdc24c23bc4b3b863ccda845df4e7398b1
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-07-22 23:29:34 +0000

    tiddlyweb - move around some documentation a bit, in an effort to clear up the top level of the distribution.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@6035 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+"""
+XXX THIS DOESN'T WORK YET! XXX
+"""
 from distutils.core import setup
 setup(name='tiddlyweb',
-        version='0.1',
-        packages=['tiddlyweb',])
+        version='0.2',
+        description='A headless wiki RESTful datastore for TiddlyWiki',
+        author='Chris Dent',
+        author_email='cdent@peermore.com',
+        packages=['tiddlyweb',],
+        scripts=['manager',],
+        )
 
