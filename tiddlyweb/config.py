1735bd0dffb3e41571f6700dd081d48896b010cf
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-08-11 14:37:23 +0000

    tiddlyweb - Provide a mechanism to override the default config. The basic idea is that if there is a file, tiddlywebconfig.py, in the current working directory (the directory from which the server starts) a dict named 'config' in that file will be incorporated into the tiddlyweb.config dict that is used throughout the application.
    tiddlywebconfig.py.atom explains this a bit more
    
    atom_server.py is no longer needed because all it was doing was setting
    the config.
    
    See tiddlyweb/config.py for details on how the merging is done. It's not
    perfect.
    
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@6288 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- tiddlyweb/config.py
+++ tiddlyweb/config.py
@@ -4,7 +4,9 @@ around in the environ. Eventually this
 be in a userside actual file.
 """
 
-config = {
+import os
+
+default_config = {
         'server_store': ['text', {'store_root': 'store'}],
         'server_host': {},
         'server_prefix': '',
@@ -38,3 +40,26 @@ A dict explaining the scheme, host and port of our server.
 FIXME: a hack to get the server.host set properly in outgoing
 wikis.
 """
+
+if os.path.exists('tiddlywebconfig.py'):
+    """
+    Read in a local configuration override, but only
+    from the current working directory (for now). If the
+    file can't be imported things will blow up and the
+    server not run.
+
+    What's expected in the override file is a dict with the
+    name config.
+    """
+    from tiddlywebconfig import config as custom_config
+    config = default_config
+    for key in custom_config:
+        try:
+            keys = custom_config[key].keys()
+            for deep_key in keys:
+                config[key].update(custom_config[key])
+        except AttributeError:
+            config[key] = custom_config[key]
+else:
+    config = default_config
+
