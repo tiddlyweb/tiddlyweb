849df38dc6325f463c7cd532a9949a6862a318a1
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-06 17:37:32 +0000

    tiddlyweb - forgot to add the test for last night's commit that adds support for dealing with characters like '/' in bag, recipe and tiddler titles.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/TiddlyWeb@7133 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ test/test_tiddler_slash.py
@@ -0,0 +1,38 @@
+"""
+Test using / in names and titles.
+"""
+
+
+import os
+import sys
+sys.path.append('.')
+
+from fixtures import reset_textstore, teststore
+
+from tiddlyweb.bag import Bag
+from tiddlyweb.tiddler import Tiddler
+
+def setup_module(module):
+    reset_textstore()
+    module.store = teststore()
+    module.bag = Bag('bag/puss')
+    module.store.put(module.bag)
+
+def test_tiddler_title_with_slash():
+    tiddler = Tiddler('hello/monkey')
+    tiddler.bag = 'bag/puss'
+    tiddler.text = 'artifice'
+
+    assert tiddler.title == 'hello/monkey'
+
+    store.put(tiddler)
+
+    tiddler2 = Tiddler('hello/monkey')
+    tiddler2.bag = 'bag/puss'
+
+    store.get(tiddler2)
+
+    assert tiddler2.title == 'hello/monkey'
+    assert tiddler2.text == 'artifice'
+
+
