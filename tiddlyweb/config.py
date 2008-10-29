f2436cb1db02b5970a0026d1410f0b68fcd74475
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-29 21:41:14 +0000

    tiddlyweb - update tiddlyweb.config to document all the common keys in the config file. Although this doesn't actually explain how to take advantage of the keys, it does give a good hint that you can take advantage.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/core@7477 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- tiddlyweb/config.py
+++ tiddlyweb/config.py
@@ -1,7 +1,106 @@
 """
 The System's Configuration, to be carried
-around in the environ. Eventually this
-be in a userside actual file.
+around in the environ as 'tiddlyweb.config'.
+
+If there is a tiddlywebconfig.py
+file in the working directory where twanager or the
+server is started, its values will override the defaults
+in this file.
+
+The server administrator may add additional keys
+to the config if they are useful in extensions.
+
+What follows are descriptions of the known keys.
+
+system_plugins -- A list of Python module names that
+act as plugins for the running server. At server startup
+time they are found, compiled, and the function init(config)
+is called on them, with a reference to the current config
+passed in. Use this to add functionality to the server
+that cannot be accomplised from the server, such as
+adding additional web handlers.
+
+twanager_plugins -- A list of Python module names that
+act as plugins for twanager, adding command line functionality.
+As with system_plugins init(config) is called.
+
+server_store -- The name of a module implementing 
+tiddlyweb.stores.StorageInterface. First the module name
+is looked up in the tiddlyweb.stores package space, if
+it is not found there, sys.path is searched. Following the
+name of the module is an arbitrary dictionary of configuration
+information that is passed to the store. This could, for
+example, include databse username and password information.
+
+server_request_filters -- A list of WSGI applications 
+which, in order, process the incoming requests made to the
+server. This can extract, add, or filter information as 
+necessary.
+
+server_response_filters -- A list of WSGI applications
+which, in order, process the outgoing response from the 
+server. This can transform, log, or handle exceptions as
+necessary.
+
+server_host -- The hostname of this server, usually set
+from whatever starts the server.
+
+server_prefix -- A URL path portion which is a prefix to
+every URL the system uses and produces. Use this to host
+TiddlyWeb in a subdirectory. Default is ''.
+
+extension_types -- A dictionary that pairs extension
+strings used in URLs as human controlled content-negotiation
+with the MIME types they represent. Add to this if you 
+add to serializers.
+
+serializers -- Incoming request Accept headers, or extension
+MIME types paired with a tiddlyweb.serializations.Serializer
+implementation and an outgoing MIME type for that type of 
+serialization.
+
+extractors -- A extractor is a credential extractor (see
+tiddlyweb.web.extractors.ExtractorInterface) that looks in
+an incoming request to attempt to extract information from
+it that indicates a potential user in the system. This 
+config item is an ordered list of extractors, tried in 
+succession until one returns tiddlyweb.usersign information
+or there are no more left.
+
+auth_systems -- A list of challengers available to the 
+system when it needs to ask for a user. (See
+tiddlyweb.web.challengers.ChallengerInterface) If there
+is more than one challenger the user is presented with a
+list of those available. If there is only one, the user
+is automatically directed to just that one. A challenger
+needs to work with the extractors system so that the 
+challenger puts something in future requests that the 
+extractor can extract.
+
+secret -- A string used to encrypt the cookie installed by
+some of the challengers and used by the cookie extractor.
+NOTE: EVERY INSTALLATION SHOULD CHANGE THIS IN ITS OWN
+CONFIGURATION.
+
+base_tiddlywiki -- the file location of the tiddlywiki
+file into which Tiddlers are pushed when creating
+outgoing TiddlyWiki representations from TiddlyWeb. This
+can be an absolute path or relative to the startup 
+directory of the server.
+
+urls_map -- the file location of the text file that maps
+URL paths to python code, doing method dispatch. With
+the advent of system_plugins, it is not often necessary
+to change this.
+
+bag_create_policy -- A policy statement on who or what
+kind of user can create new bags on the system through the
+web API. ANY means any authenticated user can. ADMIN means 
+any user with role ADMIN can. '' means anyone can.
+
+recipe_create_policy -- A policy statement on who or what kind
+of user can create new recipes on the system through the web
+API. See bag_create_policy.
 """
 
 import os
@@ -21,6 +120,7 @@ from tiddlyweb.web.wsgi import StoreSet, EncodeUTF8, SimpleLog, HTMLPresenter, P
 # wikis.
 DEFAULT_CONFIG = {
         'system_plugins': [],
+        'twanager_plugins': [],
         'server_store': ['text', {'store_root': 'store'}],
         'server_request_filters': [
             Query,
