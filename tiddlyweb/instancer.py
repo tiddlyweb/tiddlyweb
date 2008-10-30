5ee760cab349b8adf30c35ef9e907aca31fa44d6
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-30 16:05:21 +0000

    tiddlyweb - add another twanager plugin, this one adds the command 'instance' which basically does the following:   * makes a directory for your instance   * puts an empty tiddlywebconfig.py in it   * make a system bag   * puts the two usual plugins in the bag
    If the default text store is being used it is established
    in a subdirectory in the new directory.
    
    git-svn-id: http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/core@7487 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ tiddlyweb/instancer.py
@@ -0,0 +1,63 @@
+"""
+A built-in twanager plugin for setting up a new instance
+of TiddlyWeb, to lighten the load on getting started.
+
+Example:
+
+    twanager instance foo
+
+This will create a foo directory containing:
+
+    * empty tiddlywebconfig.py
+    * store directory with a system bag containing
+      the important plugins (assuming text store is
+      in default config.py)
+"""
+
+import os
+
+from tiddlyweb.fromsvn import import_list
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.manage import make_command
+from tiddlyweb.store import Store
+
+CONFIG_NAME = 'tiddlywebconfig.py'
+EMPTY_CONFIG = """# A default empty config, make your changes here.
+config = {
+}
+"""
+PLUGINS = [
+        'http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/client/plugins/TiddlyWebAdaptorPlugin.js',
+        'http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/client/plugins/TiddlyWebAutoSavePlugin.js',
+        ]
+
+@make_command()
+def instance(args):
+    """Create a tiddlyweb instance with default plugins in the named directory."""
+    directory = args[0]
+    if not directory:
+        raise ValueError('you must provide the name of a directory')
+    if os.path.exists(directory):
+        raise IOError('that name is in use')
+    os.mkdir(directory)
+    os.chdir(directory)
+    _make_bag('system')
+    import_list('system', PLUGINS)
+    _empty_config()
+
+
+def _empty_config():
+    cfg = open(CONFIG_NAME, 'w')
+    cfg.write(EMPTY_CONFIG)
+    cfg.close()
+
+
+def _make_bag(bag_name):
+    bag = Bag(bag_name)
+    store = Store(config['server_store'][0], environ={'tiddlyweb.config': config})
+    store.put(bag)
+
+
+def init(config_in):
+    global config
+    config = config_in
