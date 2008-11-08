4c20e75d04c588ffa67acddb192d26999fbf2c79
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-11-08 23:06:57 +0000

    tiddlyweb - update the mod_python.py file based on learnings from working on a mod_python installation. This version is a might be smaller, wot?
    git-svn-id: http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/core@7679 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- mod_python.py
+++ mod_python.py
@@ -5,7 +5,7 @@ customize the TiddlyWeb application and then
 provide that application (named application) as a
 callable to a WSGI handler running under mod_python.
 
-modpython_gateway can be found at
+The required modpython_gateway can be found at
 http://www.aminus.net//wiki/ModPythonGateway
 
 (There are other options as well if you are stuck
@@ -25,8 +25,9 @@ The apache configuration has something like this:
 For purposes beyond this script the name twtest and
 the path to it would need to be changed.
 
-Instead of setting config items in this script a
-tiddlywebconfig.py could be used instead.
+You will need a custom tiddlywebconfig.py to
+set some of the options used in this script.
+'hostname' and 'server_prefix' being of particular import.
 
 You will need to fill in values for some of the
 variables below.
@@ -36,38 +37,25 @@ import os
 import os.path
 import sys
 
-# Change this to the location of the tiddlyweb code,
-# if tiddlyweb is not in sys.path
-sys.path.append('/home/cdent/www/twtest/TiddlyWeb')
+# chdir to the location of this running script so we have access 
+# to tiddlywebconfig.py
+dirname = os.path.dirname(__file__)
+if dirname:
+    os.chdir(dirname)
 
 from tiddlyweb.web import serve
 from tiddlyweb.config import config
 
-# Where is the TiddlyWeb content stored
-file_store_location = '/home/cdent/www/twtest/TiddlyWeb/docstore'
-
 def start():
     # What is our hostname
-    hostname = 'plugins.tiddlywiki.org'
-
-    # What is path to us (the base url)
-    server_prefix = '/twtest'
+    hostname = config['hostname']
 
     port = 80
     if ':' in hostname:
         hostname, port = hostname.split(':')
 
-    # This assumes the default text server_store is being used.
-    # If you are not using that store, you'll need to change this.
-    config['server_store'] = ['text', {'store_root': file_store_location}]
-    config['server_prefix'] = server_prefix
-
     app = serve.load_app(hostname, port, config['urls_map'])
     return app
-
-# chdir to the location of this running script so we have access 
-# urls.map and lib/empty.html
-os.chdir(os.path.dirname(__file__))
 
 # the mod_python wsgi handler will look for the callable 
 # named application
