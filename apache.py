d05c6759216b55c6a45126a1f0cab7f6eed9ca44
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-02-17 17:54:24 +0000

    tiddlyweb - unify mod_wsgi.py and mod_python.py in apache.py because they do the same thing now. It is just the configuration that is different.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/core@8783 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

---
+++ apache.py
@@ -0,0 +1,91 @@
+"""
+This is a handler for running tiddlyweb 
+under apache using either mod_wsgi or mod_python.
+
+mod_wsgi is a better choice.
+
+##################################################
+For mod_wsgi 
+
+Your apache must be configured to use mod_wsgi.
+
+Add the following to server config (in a virtual host section)
+
+    WSGIDaemonProcess teamtasks.peermore.com user=cdent processes=2 threads=15
+    WSGIProcessGroup teamtasks.peermore.com
+    WSGIScriptAlias /t /home/cdent/public_html/teamtasks.peermore.com/apache.py
+
+Replace teamtasks.peermore.com with the hostname being used.
+Replace cdent with the user the process should run as. Remove
+    the user line if you want to use the apache user.
+Replace /t with the prefix to the tiddlyweb.
+Replace the path after /t with the path to apache.py which 
+    should live in in the tiddlyweb instance directory.
+
+##################################################
+For mod_python
+
+Your apache must be configured with mod_python.
+
+What this script does is provide a callable
+(named application) to a WSGI handler running under
+mod_python.
+
+A gateway from mod_python to WSGI is required 
+it can be found at
+http://www.aminus.net/wiki/ModPythonGateway
+
+Adjust the apache configuration something like
+the following:
+
+        <Location /t>
+                PythonPath "['/home/cdent/www/t'] + sys.path"
+                PythonOption SCRIPT_NAME /t
+                SetHandler python-program
+                PythonHandler modpython_gateway::handler
+                PythonOption wsgi.application apache::application
+        </Location>
+
+In your own setup the name twtest and the path to it
+would need to be changed.
+
+##################################################
+For both
+
+In tiddlywebconfig.py:
+
+    set server_prefix to the prefix above (/t)
+    set server_host to the scheme, hostname and port being used
+    set css_uri to a css file if you have one
+
+server_host is a complex data structure as follows:
+
+    'server_host': {
+        'scheme': 'http',
+        'hostname': 'some.example.com',
+        'port': '80',
+    }
+
+"""
+
+import os
+import sys
+
+# chdir to the location of this running script so we have access 
+# to tiddlywebconfig.py and plugins
+dirname = os.path.dirname(__file__)
+if dirname:
+    os.chdir(dirname)
+
+# you may wish to change this path
+os.environ['PYTHON_EGG_CACHE'] = '/tmp'
+sys.path.insert(0, dirname)
+
+from tiddlyweb.web import serve
+
+def start():
+    app = serve.load_app()
+    return app
+
+# apache code will look for a callable # named application
+application = start()
