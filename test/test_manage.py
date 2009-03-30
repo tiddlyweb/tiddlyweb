88935c626f87e53477384d898c3db58b6e78984d
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2009-03-30 22:10:52 +0000

    tiddlyweb - enhance test coverage of some of the twanager commands: bag, recipe and tiddler. We case StringIO to fake stdin before we go into the methods.
    git-svn-id: http://svn.tiddlywiki.org/Trunk/association/serversides/tiddlyweb/core@9343 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- test/test_manage.py
+++ test/test_manage.py
@@ -4,27 +4,96 @@ Test the manager a little bit.
 
 import os
 import sys
-import StringIO
+from StringIO import StringIO
 
-from tiddlyweb.manage import handle
 from fixtures import reset_textstore
+
+from tiddlyweb.config import config
+from tiddlyweb.manage import handle
+from tiddlyweb.store import Store
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.recipe import Recipe
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.model.user import User
+
+BAG_STRING = """
+{"desc": "hello", "policy": {}}
+"""
+
+RECIPE_STRING = """/bags/bag1/tiddlers
+/bags/bag2/tiddlers
+"""
+
+TIDDLER_STRING = """modifier: cdent
+
+Hello!
+"""
 
 
 def setup_module(module):
+    reset_textstore()
     module.savedout = sys.stdout
-    module.output = StringIO.StringIO()
+    module.savedin = sys.stdin
+    module.output = StringIO()
     sys.stdout = output
+    module.store = Store(config['server_store'][0], environ={'tiddlyweb.config': config})
+
 
 def teardown_module(module):
     content = module.output.getvalue()
     sys.stdout = module.savedout
+    sys.stdin = module.savedin
+
 
 def test_help():
     handle('help')
     results = output.getvalue()
     assert 'Add or update a user to the database' in results
 
+
 def test_adduser():
     handle(['', 'adduser', 'cdent', 'crunk'])
-    assert os.path.exists(os.path.join('store','users','cdent'))
+    the_user = User('cdent')
+    the_user = store.get(the_user)
+    assert the_user.check_password('crunk')
+
+
+def test_bag():
+    set_stdin(BAG_STRING)
+    handle(['', 'bag', 'bag1'])
+
+    the_bag = Bag('bag1')
+    the_bag = store.get(the_bag)
+
+    assert the_bag.name == 'bag1'
+    assert the_bag.desc == 'hello'
+
+
+def test_recipe():
+    set_stdin(RECIPE_STRING)
+    handle(['', 'recipe', 'recipe1'])
+
+    the_recipe = Recipe('recipe1')
+    the_recipe = store.get(the_recipe)
+
+    assert the_recipe.name == 'recipe1'
+    assert u'bag1' in the_recipe.get_recipe()[0]
+    assert u'bag2' in the_recipe.get_recipe()[1]
+
+
+def test_tiddler():
+    set_stdin(TIDDLER_STRING)
+    handle(['', 'tiddler', 'tiddler1', 'bag1'])
+
+    the_tiddler = Tiddler('tiddler1', 'bag1')
+    the_tiddler = store.get(the_tiddler)
+
+    assert the_tiddler.title == 'tiddler1'
+    assert the_tiddler.bag == 'bag1'
+    assert the_tiddler.modifier == 'cdent'
+
+
+def set_stdin(content):
+    f = StringIO(content)
+    sys.stdin = f
 
