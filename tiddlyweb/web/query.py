3c89f3f72aaa12be19f5ab24da0994340eeafac8
Chris Dent <cdent@matter.local>
2009-05-05 13:59:53 +0100

    experiment with parsing out new style filter query parameters from the query string
    This results in a list of ordered filters in environ['tiddlyweb.filters'] and
    the left query parameters in environ['tiddlyweb.query'].
    
    For now the filters are just strings, but as things evolve they will be
    functions.

--- tiddlyweb/web/query.py
+++ tiddlyweb/web/query.py
@@ -7,6 +7,24 @@ cgi.py users (dictionary of lists).
 
 import cgi
 
+def select_parse(command):
+    return 'select ' + command
+
+
+def sort_parse(command):
+    return 'sort ' + command
+
+
+def limit_parse(command):
+    return 'limit ' + command
+
+
+FILTER_PARSERS = {
+        'select': select_parse,
+        'sort':   sort_parse,
+        'limit':  limit_parse,
+        }
+
 
 class Query(object):
     """
@@ -34,5 +52,27 @@ class Query(object):
             content = environ['wsgi.input'].read(int(length))
             posted_data = cgi.parse_qs(content)
             environ['tiddlyweb.query'].update(posted_data)
-        environ['tiddlyweb.query'].update(cgi.parse_qs(
-            environ.get('QUERY_STRING', '')))
+        parse_for_filters(environ)
+
+
+def parse_for_filters(environ):
+    query_string = environ.get('QUERY_STRING', '')
+    if ';' in query_string:
+        strings = query_string.split(';')
+    else:
+        strings = query_string.split('&')
+
+    filters = []
+    leftovers = [] 
+    for string in strings:
+        query = cgi.parse_qs(string)
+        try:
+            key, value = query.items()[0]
+            func = FILTER_PARSERS[key](value[0])
+            filters.append(func)
+        except(KeyError, IndexError):
+            leftovers.append(string)
+
+    leftovers = ';'.join(leftovers)
+    environ['tiddlyweb.query'].update(cgi.parse_qs(leftovers))
+    environ['tiddlyweb.filters'] = filters
