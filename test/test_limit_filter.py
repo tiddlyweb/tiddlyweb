75896b51bfd40f0da9d4ebf2b9b68270e07af1de
Chris Dent <cdent@matter.local>
2009-05-07 14:58:28 +0100

    First stab at implementing the limit filter. This one is pretty straightforward: we take a list of tiddlers and return a slice.

---
+++ test/test_limit_filter.py
@@ -0,0 +1,22 @@
+"""
+Test the abilities of the limit filter.
+This is not about query parsing, but rather
+handling once we have the filter.
+"""
+
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.filters.limit import limit
+
+tiddlers = [Tiddler('1'), Tiddler('c'), Tiddler('a'), Tiddler('b')]
+
+def test_simple_limit():
+    limited_tiddlers = limit(tiddlers, count=2)
+
+    assert len(limited_tiddlers) == 2
+    assert ['1','c'] == [tiddler.title for tiddler in limited_tiddlers]
+
+
+def test_ranged_limit():
+    limited_tiddlers = limit(tiddlers, index=1, count=2)
+
+    assert ['c','a'] == [tiddler.title for tiddler in limited_tiddlers]
---
+++ tiddlyweb/filters/limit.py
@@ -0,0 +1,4 @@
+
+def limit(tiddlers, count=0, index=0):
+    return tiddlers[index:index+count]
+
