03ce95021fce97fd542ca12658f8bc1140231747
Chris Dent <cdent@matter.lan>
2009-05-07 21:28:59 +0100

    Move filter parsing code out of the wsgi query processor to the filters code.

--- tiddlyweb/web/query.py
+++ tiddlyweb/web/query.py
@@ -7,64 +7,7 @@ cgi.py users (dictionary of lists).
 
 import cgi
 
-from tiddlyweb.filters.select import select_by_attribute, select_relative_attribute
-from tiddlyweb.filters.sort import sort_by_attribute
-from tiddlyweb.filters.limit import limit
-
-
-def select_parse(command):
-    attribute, args = command.split(':', 1)
-
-    if args.startswith('!'):
-        args = args.replace('!', '', 1)
-        def selector(tiddlers):
-            return select_by_attribute(attribute, args, tiddlers, negate=True)
-    elif args.startswith('<'):
-        args = args.replace('<', '', 1)
-        def selector(tiddlers):
-            return select_relative_attribute(attribute, args, tiddlers, lesser=True)
-    elif args.startswith('>'):
-        args = args.replace('>', '', 1)
-        def selector(tiddlers):
-            return select_relative_attribute(attribute, args, tiddlers, greater=True)
-    else:
-        def selector(tiddlers):
-            return select_by_attribute(attribute, args, tiddlers)
-
-    return selector
-
-
-def sort_parse(attribute):
-    attribute
-
-    if attribute.startswith('-'):
-        attribute = attribute.replace('-', '', 1)
-        def sorter(tiddlers):
-            return sort_by_attribute(attribute, tiddlers, reverse=True)
-    else:
-        def sorter(tiddlers):
-            return sort_by_attribute(attribute, tiddlers)
-
-    return sorter
-
-
-def limit_parse(count='0'):
-    index = '0'
-    if ',' in count:
-        index, count = count.split(',', 1)
-    index = int(index)
-    count = int(count)
-    def limiter(tiddlers):
-        return limit(tiddlers, index=index, count=count)
-
-    return limiter
-
-
-FILTER_PARSERS = {
-        'select': select_parse,
-        'sort':   sort_parse,
-        'limit':  limit_parse,
-        }
+from tiddlyweb.filters import FILTER_PARSERS
 
 
 class Query(object):
