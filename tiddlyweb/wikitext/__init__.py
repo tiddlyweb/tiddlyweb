8989aa58e47e9b4a96debd83e24738e6b007a97a
Chris Dent <cdent@matter.local>
2009-06-28 15:56:14 +0100

    Extract the renderer determination code to it own method for future expandability.

--- tiddlyweb/wikitext/__init__.py
+++ tiddlyweb/wikitext/__init__.py
@@ -8,8 +8,19 @@ def render_wikitext(tiddler=None, path='', environ={}):
 
     container_path is used when generating URLs
     """
-    renderer_name = environ.get('tiddlyweb.config', {}).get('wikitext_renderer', DEFAULT_RENDERER)
-    imported_module = __import__('tiddlyweb.wikitext.%s' % renderer_name, {}, {}, ['render'])
+    renderer_name = _determine_renderer(tiddler, environ)
+    try:
+        imported_module = __import__('tiddlyweb.wikitext.%s' % renderer_name,
+                {}, {}, ['render'])
+    except ImportError, err:
+        err1 = err
+        try:
+            imported_module = __import__(renderer, {}, {}, ['render'])
+        except ImportError, err:
+            raise ImportError("couldn't load module for %s: %s, %s" % (render, err, err1))
     return imported_module.render(tiddler, path, environ)
 
+
+def _determine_renderer(tiddler, environ):
+    return environ.get('tiddlyweb.config', {}).get('wikitext_renderer', DEFAULT_RENDERER)
 
