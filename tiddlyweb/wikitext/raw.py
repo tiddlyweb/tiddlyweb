7a9fd255d8f4dba6e8632dc58431fdb1cee27147
Chris Dent <cdent@host49.msm.che.vodafone>
2009-07-01 14:54:42 +0100

    Check in the raw wikitext renderer which was created a while ago, but got lost along the way.

---
+++ tiddlyweb/wikitext/raw.py
@@ -0,0 +1,11 @@
+"""
+Wikitext renderer which does not render the wikitext
+but instead wraps it in pre tags.
+"""
+from tiddlyweb.web.util import html_encode
+
+def render(tiddler, path, environ):
+    """
+    Wrap html encoded wikitext with pre tags.
+    """
+    return '<pre>\n' + html_encode(tiddler.text) + '</pre>\n'
