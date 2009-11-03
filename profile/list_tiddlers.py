1ecc3c9ee79004fe5fb38b65a8ab2bc669100dc7
Chris Dent <cdent@oxide.local>
2009-11-03 17:22:35 +0000

    Update the list_tiddlers profiler to separate out cleaning, building and profiling the data.

--- profile/list_tiddlers.py
+++ profile/list_tiddlers.py
@@ -13,6 +13,7 @@ from tiddlyweb.model.tiddler import Tiddler
 from tiddlyweb.store import Store
 from tiddlyweb import control
 from tiddlyweb.serializer import Serializer
+from tiddlyweb.filters import parse_for_filters
 
 environ = {}
 environ['tiddlyweb.config'] = config
@@ -24,7 +25,10 @@ def run():
 
 
 def clean_store():
-    shutil.rmtree('store')
+    try:
+        shutil.rmtree('store')
+    except OSError:
+        pass
 
 def make_tiddlers_for_bag():
     store = Store('text', environ=environ)
@@ -33,38 +37,52 @@ def make_tiddlers_for_bag():
     bag = Bag('profiler')
     store.put(bag)
 
-    for name in range(1, 1000):
+    for name in range(1, 10000):
+        tag = name % 10
         name = str(name)
+        tag = str(tag)
         tiddler = Tiddler(name, bag.name)
         tiddler.text = name
+        tiddler.tags.append(tag)
         store.put(tiddler)
     print 'stored', time()
 
 def profile_listing_tiddlers():
     store = Store('text', environ=environ)
+    environ['tiddlyweb.store'] = store
 
     bag = Bag('profiler')
+    bag.skinny = True
     store.get(bag)
 
     print 'filter', time()
-    #filter_string = '[sort[modified]]'
-    filter_string = ''
-    tiddlers = control.filter_tiddlers_from_bag(bag, filter_string)
+    filter_string = 'select=tag:100'
+    filters, leftovers = parse_for_filters(filter_string, environ)
+    tiddlers = control.filter_tiddlers_from_bag(bag, filters)
 
     print 'tmp bag', time()
     tmp_bag = Bag('tmp_bag', tmpbag=True)
     tmp_bag.add_tiddlers(tiddlers)
 
-    #print 'output', time()
-    #print ['.' for tiddler in control.get_tiddlers_from_bag(tmp_bag)]
+    print 'output', time()
+    print [tiddler.title for tiddler in control.get_tiddlers_from_bag(tmp_bag)]
 
-    print 'serializer', time()
-    serializer = Serializer('wiki', environ)
-    print 'wikify', time()
-    output = serializer.list_tiddlers(tmp_bag)
+    #print 'serializer', time()
+    #serializer = Serializer('wiki', environ)
+    #print 'wikify', time()
+    #output = serializer.list_tiddlers(tmp_bag)
 
     print 'done', time()
 
 
 if __name__ == '__main__':
-    run()
+    command = sys.argv[1]
+    print command
+    if command == 'clean':
+        clean_store()
+    elif command == 'make':
+        make_tiddlers_for_bag()
+    elif command == 'filter':
+        profile_listing_tiddlers()
+    else:
+        run()
