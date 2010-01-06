b2c41c4ff93b9436893de0313781dc63fb588c9b
Chris Dent <chris.dent@gmail.com>
2010-01-06 12:56:37 +0000

    prep a 0.9.89 release

--- tiddlyweb/__init__.py
+++ tiddlyweb/__init__.py
@@ -30,7 +30,7 @@ TiddlyWeb includes twanager, a command line tool for
 doing a variety of TiddlyWeb activities. Run twanager
 without arguments for a list of commands.
 """
-__version__ = '0.9.88'
+__version__ = '0.9.89'
 __author__ = 'Chris Dent (cdent@peermore.com)'
 __copyright__ = 'Copyright UnaMesa Association 2008-2010'
 __contributors__ = ['Frederik Dohr',
