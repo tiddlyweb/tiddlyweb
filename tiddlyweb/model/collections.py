4868cbd72ebc95ac0fe5e87090b4c47f2bca558c
Chris Dent <cdent@oxide.local>
2010-03-19 20:57:59 +0000

    Adjustments required by pylint.

--- tiddlyweb/model/collections.py
+++ tiddlyweb/model/collections.py
@@ -1,10 +1,21 @@
 """
 Collection classes.
+
+These classes are used for containing the other
+model classes. Because the main reason for having a
+collection is to send it out over the web, the
+collections keep track of their last-modified time
+and generate a hash suitable for use as an ETag.
 """
 
 from tiddlyweb.util import sha
 
 class Collection(object):
+    """
+    Base class for all collections.
+
+    Can be used directly for random stuff is required.
+    """
 
     def __init__(self):
         self._digest = sha()
@@ -15,6 +26,10 @@ class Collection(object):
         return item in self._container
 
     def add(self, thing):
+        """
+        Add an item to the container, updating
+        the digest and modified information.
+        """
         self._update_digest(thing)
         self._container.append(thing)
         try:
@@ -24,23 +39,42 @@ class Collection(object):
             pass
 
     def _update_digest(self, thing):
+        """
+        Update the digest with this thing.
+        """
         self._digest.update(thing)
 
     def hexdigest(self):
+        """
+        Return the current hex representation of
+        the hash digest of this collection.
+        """
         return self._digest.hexdigest()
 
     def out(self):
+        """
+        Generate the items in this container.
+        """
         for thing in self._container:
             yield thing
 
 class Tiddlers(Collection):
+    """
+    A Collection specifically for tiddlers.
 
+    This differs from the base class in how
+    the digest is calculated. tiddler.title and
+    either tiddler.bag or tiddler.recipe are used.
+    """
     def __init__(self):
         Collection.__init__(self)
         self.is_revisions = False
         self.is_search = False
 
     def _update_digest(self, tiddler):
+        """
+        Update the digest with information from this tiddler.
+        """
         if tiddler.recipe:
             container = tiddler.recipe
         elif tiddler.bag:
