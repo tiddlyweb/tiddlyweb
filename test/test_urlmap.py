a49c5cb709b4b8c630b45e350898f273d52222aa
Chris Dent <chris.dent@gmail.com>
2010-06-09 17:34:08 +0100

    First stab at generating HTTP tests directly from the urls.map
    This is done by processing the data structure created by selector
    to build up a list of dictionaries in the same form as used by
    test/http_runner.py.
    
    Those tests are then sorted and filled out in rather arbitrary
    and currently much too baroque ways to get them to run to satisfactory
    completion.
    
    The hope is that some of that baroqueness can be be clarified by
    doing more declarative work, probably by making the urls.map file
    itself more declarative and skipping the selector processing
    entirely.
    
    Some caveats:
    
    * it's JSON everywhere
    * the challenger uris are skipped
    * we do all the PUTs, then all the GETs, then all the DELETEs
    * we go from shortest url to longest in each group, so that
      recipes and bags are in place before tiddlers. We go in the
      opposite direction for DELETEs
    * the data involved is dead simple so there's no real testing
      of the serializations or policies or such like
    
    The expectation is that this is a tool to make sure things are
    running at their very basic level, nothing more than that.

---
+++ test/test_urlmap.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import selector
+import string
+import simplejson
+
+from http_runner import *
+
+def fixup(pattern):
+    for replaceme in ['^', '\\', '(', ')', '?', '$', '[/]+', '[/.]+', '>']:
+        pattern = pattern.replace(replaceme, '')
+    pattern = pattern.replace('P<', '$')
+    pattern = pattern.replace('.$format', '')
+    return pattern
+
+
+def filled_pattern(pattern):
+    template = string.Template(pattern)
+    info = {
+            'recipe_name': 'recipeurlmap1',
+            'tiddler_name': 'tiddlerurlmap1',
+            'bag_name': 'bagurlmap1',
+            'revision': '1',
+            'challenger': 'cookie_form',
+            }
+    return template.substitute(**info)
+
+
+def data_from_pattern(pattern):
+    data = {}
+    if '$recipe_name' in pattern:
+        if '$tiddler_name' in pattern:
+            data['text'] = 'oh hai'
+        else:
+            data['recipe'] = [
+                    ['bagurlmap1', ''],
+                    ]
+    return simplejson.dumps(data)
+
+
+def figure_tests(app):
+    tests = []
+    for pattern, methods in app.mappings:
+        pattern = fixup(pattern.pattern)
+        for method_type in methods:
+            test = {}
+            test['method'] = method_type
+            test['name'] = '%s:%s' % (method_type, pattern)
+            test['desc'] = test['name']
+            test['url'] = filled_pattern(pattern)
+            if 'challenge' in test['url']:
+                continue
+            if 'search' in test['url']:
+                test['url'] = test['url'] + '?q=hai'
+                test['expected'] = ['tiddlerurlmap1']
+            if method_type == 'GET':
+                test['status'] = '200'
+                test['request_headers'] = {
+                        'accept': 'application/json',
+                        }
+            elif method_type == 'DELETE':
+                test['status'] = '204'
+                if 'recipe_name' in pattern and 'tiddler_name' in pattern:
+                    continue
+            elif method_type == 'PUT':
+                test['status'] = '204'
+                test['data'] = data_from_pattern(pattern)
+                test['request_headers'] = {
+                        'content-type': 'application/json',
+                        }
+            else:
+                continue
+            print test['url']
+            tests.append(test)
+    def test_sorter(test):
+        method = test['method']
+        if method == 'DELETE':
+            key = test['method'] + 'x' * len(test['name'])
+        else:
+            key = test['method'] + 'x' * (70 - len(test['name']))
+        return key
+    return sorted(tests, key=test_sorter, reverse=True)
+
+
+def setup_module(module):
+    tests = do_run()
+    #print tests
+    http_test(tests, 'http://our_test_domain:8001')
+
+
+def do_run():
+    app = selector.Selector(mapfile='tiddlyweb/urls.map')
+    return figure_tests(app)
+
+
+if __name__ == '__main__':
+    do_run()
+
