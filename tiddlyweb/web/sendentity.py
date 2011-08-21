d10df993faf71659d8f722ed6028464aabb8ff5e
Chris Dent <chris.dent@gmail.com>
2011-08-21 12:34:32 +0100

    refactor etag handling for single entities to make them more cacheable
    * move etag generation into tiddlyweb.web.util
    * remove mime-types in etag generation: we Vary on Accept
      so caches will adapt correctly (by including Accept in cache
      identifiers)
    * use the JSON serialization to create the string from which a
      hash is created
    
    This last is the major change. For bags and recipes the hash
    already came from a pseudo serialization (python repr strings)
    so using JSON allows things to be more correct and have less
    duplication. For tiddlers the text attribute is set to null before
    the serialization is done and we get benefits:
    
    * server.perms are automatically included in the hash digest,
      meaning we can have different etags for different users without
      needing to refer to the indvidual user. This means that two users
      with the same permissions will get the same etag, reducing the total
      number of etags out there, increasing the chances of cache hits.

--- tiddlyweb/web/sendentity.py
+++ tiddlyweb/web/sendentity.py
@@ -5,8 +5,7 @@ This consolidates common code for bags and recipes.
 
 from tiddlyweb.serializer import Serializer, NoSerializationError
 from tiddlyweb.web.http import HTTP415
-from tiddlyweb.web.util import get_serialize_type, encode_name
-from tiddlyweb.util import sha
+from tiddlyweb.web.util import get_serialize_type, entity_etag
 
 
 def send_entity(environ, start_response, entity):
@@ -14,7 +13,6 @@ def send_entity(environ, start_response, entity):
     Send a bag or recipe out HTTP, first serializing to
     the correct type.
     """
-    username = environ['tiddlyweb.usersign']['name']
     try:
         serialize_type, mime_type = get_serialize_type(environ)
         serializer = Serializer(serialize_type, environ)
@@ -23,8 +21,7 @@ def send_entity(environ, start_response, entity):
     except NoSerializationError:
         raise HTTP415('Content type %s not supported' % mime_type)
 
-    etag_string = '"%s"' % (sha(_entity_etag(entity) +
-        encode_name(username) + encode_name(mime_type)).hexdigest())
+    etag_string = entity_etag(environ, entity)
 
     start_response("200 OK",
             [('Content-Type', mime_type),
@@ -35,20 +32,3 @@ def send_entity(environ, start_response, entity):
         return [content]
     else:
         return content
-
-
-def _entity_etag(entity):
-    """
-    Generate a unique hash from the contents of the entity.
-    """
-    etag = ''
-    etag += entity.name
-    for attr in entity.policy.attributes:
-        etag += '%s' % getattr(entity.policy, attr)
-    try:
-        for item in entity:
-            etag += '%s' % item
-    except TypeError:
-        # entity not a iterator
-        pass
-    return etag.encode('utf-8')
