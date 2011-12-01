5f4b6f6e546330705160c423b48859dc690aaa56
Chris Dent <chris.dent@gmail.com>
2011-12-01 20:47:41 +0000

    Fully cover readable_tiddlers_by_bag
    This includes a tiddler in a bag that does not exist. That
    shows up in the results because a bag that does not exist is a proto-bag
    with the default policy (readable).

--- test/test_control.py
+++ test/test_control.py
@@ -8,7 +8,8 @@ import py.test
 from tiddlyweb.config import config
 from tiddlyweb.store import Store, NoBagError
 from tiddlyweb.control import (determine_bag_for_tiddler,
-        get_tiddlers_from_recipe, determine_bag_from_recipe)
+        get_tiddlers_from_recipe, determine_bag_from_recipe,
+        readable_tiddlers_by_bag)
 from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.recipe import Recipe
 from tiddlyweb.model.tiddler import Tiddler
@@ -85,3 +86,37 @@ def test_index_query_in_recipe():
     tiddler = Tiddler('dwell')
     bag = determine_bag_from_recipe(recipe, tiddler, environ)
     assert bag.name == 'noop'
+
+
+def test_readable_tiddlers_by_bag():
+    bagone = Bag('cdentread')
+    bagone.policy.read = ['cdent']
+    store.put(bagone)
+    bagtwo = Bag('fndread')
+    bagtwo.policy.read = ['fnd']
+    store.put(bagtwo)
+    bagthree = Bag('allread')
+    store.put(bagthree)
+
+    count = 0
+    tiddlers = []
+    for bag in ['cdent', 'fnd', 'all', 'cdent', 'fnd', 'all', 'notreal']:
+        count += 1
+        tiddler = Tiddler('tiddler%s' % count, '%sread' % bag)
+        tiddler.text = 'narf'
+        tiddlers.append(tiddler)
+
+    usersign = {'name': 'cdent', 'roles': []}
+    readable = list(readable_tiddlers_by_bag(store, tiddlers, usersign))
+    assert (['tiddler1', 'tiddler3', 'tiddler4', 'tiddler6', 'tiddler7'] ==
+            [tiddler.title for tiddler in readable])
+
+    usersign = {'name': 'fnd', 'roles': []}
+    readable = list(readable_tiddlers_by_bag(store, tiddlers, usersign))
+    assert (['tiddler2', 'tiddler3', 'tiddler5', 'tiddler6', 'tiddler7'] ==
+            [tiddler.title for tiddler in readable])
+
+    usersign = {'name': 'GUEST', 'roles': []}
+    readable = list(readable_tiddlers_by_bag(store, tiddlers, usersign))
+    assert (['tiddler3', 'tiddler6', 'tiddler7'] ==
+            [tiddler.title for tiddler in readable])
