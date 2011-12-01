34f4c1c770b634bb3a07f9ac6d4f389f5a469a26
Chris Dent <chris.dent@gmail.com>
2011-12-01 19:10:15 +0000

    Cover the instance where the bag listed in a recipe is a bag object.
    This is not a "best practice" but is supported in control, so
    good to have a test for it.

--- test/test_control.py
+++ test/test_control.py
@@ -7,9 +7,18 @@ import py.test
 
 from tiddlyweb.config import config
 from tiddlyweb.store import Store, NoBagError
-from tiddlyweb.control import determine_bag_for_tiddler
+from tiddlyweb.control import (determine_bag_for_tiddler,
+        get_tiddlers_from_recipe)
+from tiddlyweb.model.bag import Bag
 from tiddlyweb.model.recipe import Recipe
 from tiddlyweb.model.tiddler import Tiddler
+
+
+def setup_module(module):
+    module.environ = {'tiddlyweb.config': config}
+    module.store = Store(config['server_store'][0], config['server_store'][1],
+            environ=module.environ)
+
 
 def test_determine_bag_for_tiddler():
     recipe = Recipe('example')
@@ -31,3 +40,21 @@ def test_determine_bag_for_tiddler():
         ('bagtwo', 'select=title:monkeys')])
 
     py.test.raises(NoBagError, 'determine_bag_for_tiddler(recipe, tiddler)')
+
+
+def test_bag_object_in_recipe():
+    bag = Bag('fwoop')
+    store.put(bag)
+    tiddler = Tiddler('swell', 'fwoop')
+    tiddler.text = 'hi'
+    store.put(tiddler)
+
+    recipe = Recipe('heyo')
+    recipe.set_recipe([(bag, '')])
+    recipe.store = store
+    tiddlers = list(get_tiddlers_from_recipe(recipe, environ))
+    assert len(tiddlers) == 1
+    assert tiddlers[0].title == 'swell'
+    assert tiddlers[0].bag == 'fwoop'
+
+
