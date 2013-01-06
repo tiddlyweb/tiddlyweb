9a684b6abe35695003e1a4751b1dbf6c166a7669
Chris Dent <chris.dent@gmail.com>
2013-01-06 12:45:21 +0000

    avoid misrepresentation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from tiddlyweb import __version__ as VERSION
 
 setup(name = 'tiddlyweb',
         version = VERSION,
-        description = 'An optionally headless, extensible RESTful datastore for tiddlers: bits of stuff.',
+        description = 'An optionally headless, extensible HTTP datastore for tiddlers: bits of stuff.',
         long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
         author = 'Chris Dent',
         author_email = 'cdent@peermore.com',
