eb7c85cb8096996ee353f2952c5177d1e3655625
FND <fnd@localhost.localdomain>
2013-01-15 17:14:09 +0100

    made dependencies accessible to the outside
    I'm not actually certain that the `__name__` check in `setup.py` will
    work across all scenarios - I suppose we'll just have to try it
    
    in the process, updated some documentation; pytest is now a separate
    library

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Setup file for packaging TiddlyWeb.
+Setup file for packaging TiddlyWeb
 """
 
 import os
@@ -8,23 +8,32 @@ from setuptools import setup, find_packages
 
 from tiddlyweb import __version__ as VERSION
 
-setup(name = 'tiddlyweb',
-        version = VERSION,
-        description = 'An optionally headless, extensible HTTP datastore for tiddlers: bits of stuff.',
-        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
-        author = 'Chris Dent',
-        author_email = 'cdent@peermore.com',
-        url = 'http://pypi.python.org/pypi/tiddlyweb',
-        packages = find_packages(exclude=['test', 'test.*', 'profile']),
-        scripts = ['twanager',],
-        platforms = 'Posix; MacOS X; Windows',
-        install_requires = ['setuptools',
-            'httpexceptor',
-            'selector',
-            'simplejson',
-            'html5lib',
-            'mimeparse',
-            'cherrypy'],
-        include_package_data = True,
-        zip_safe = False,
-        )
+
+META = {
+    'name': 'tiddlyweb',
+    'version': VERSION,
+    'description': 'An optionally headless, extensible HTTP datastore for tiddlers: bits of stuff.',
+    'long_description': file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    'author': 'Chris Dent',
+    'author_email': 'cdent@peermore.com',
+    'url': 'http://pypi.python.org/pypi/tiddlyweb',
+    'packages': find_packages(exclude=['test', 'test.*', 'profile']),
+    'scripts': ['twanager'],
+    'platforms': 'Posix; MacOS X; Windows',
+    'install_requires': ['setuptools',
+        'httpexceptor',
+        'selector',
+        'simplejson',
+        'html5lib',
+        'mimeparse',
+        'cherrypy'],
+    'extras_require': {
+        'testing': ['pytest', 'httplib2', 'wsgi_intercept', 'PyYAML',
+                'tiddlywebplugins.utils']
+    },
+    'include_package_data': True,
+    'zip_safe': False,
+}
+
+if __name__ == '__main__':
+    setup(**META)
