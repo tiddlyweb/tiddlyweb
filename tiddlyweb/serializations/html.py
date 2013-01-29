6c350b639ccfb99830cc4495671ea9a26d072aea
Chris Dent <chris.dent@gmail.com>
2013-01-29 15:26:38 +0000

    Deal with a rare encoding problem in HTML serialization
    When listing tiddlers if the query string is not url escaped
    we need to make sure we get it to the desired form.

--- tiddlyweb/serializations/html.py
+++ tiddlyweb/serializations/html.py
@@ -250,7 +250,7 @@ class Serialization(SerializationInterface):
             links = []
             query_string = self.environ.get('QUERY_STRING', '')
             if query_string:
-                query_string = '?%s' % query_string
+                query_string = '?%s' % encode_name(query_string.decode('utf-8'))
             for extension in extension_types:
                 link = '<a href="%s.%s%s">%s</a>' % (representation_link,
                         extension, query_string, extension)
