e549fce5728cab797a69f160db726f4ea0fa60da
Chris Dent <chris.dent@gmail.com>
2013-05-28 15:30:47 +0100

    A (fully) failing test for 304 handling
    This tests those resources which might can return a 304 to confirm
    that the response includes all those headers which httpbis says
    it must (or should):
    
       http://tools.ietf.org/html/draft-ietf-httpbis-p4-conditional-22#section-4.1
    
    In our case these headers are listed in RELEVANT_HEADERS in the
    test file. We don't include content-location because we aren't
    using that at the moment.
    
    This if part of issue #77
    
    This fails at the moment, as expected. Now to make the code
    align with the tests.
    
    /cc @pads @fnd

---
+++ test/test_web_304_handling.py
@@ -0,0 +1,90 @@
+
+import httplib2
+import random
+import string
+
+from .fixtures import reset_textstore, initialize_app, _teststore
+
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.recipe import Recipe
+from tiddlyweb.model.tiddler import Tiddler
+
+
+RELEVANT_HEADERS = [
+        'cache-control',
+        'etag',
+        'vary',
+        'last-modified']
+
+
+def setup_module(module):
+    initialize_app()
+    reset_textstore()
+    module.store = _teststore()
+    http = httplib2.Http()
+    module.http = http
+
+
+def _random_name(length=5):
+        return ''.join(random.choice(string.lowercase) for i in range(length))
+
+
+def test_get_tiddler():
+    bag = Bag(_random_name())
+    store.put(bag)
+
+    tiddler = Tiddler(_random_name(), bag.name)
+    tiddler.text = _random_name(10)
+    store.put(tiddler)
+
+    _get_entity('http://our_test_domain:8001/bags/%s/tiddlers/%s' % (
+        bag.name, tiddler.title))
+
+
+def test_get_bag():
+    bag = Bag(_random_name())
+    store.put(bag)
+    _get_entity('http://our_test_domain:8001/bags/%s' % bag.name)
+
+
+def test_get_recipe():
+    recipe = Recipe(_random_name())
+    store.put(recipe)
+    _get_entity('http://our_test_domain:8001/recipes/%s' % recipe.name)
+
+
+def test_get_tiddlers():
+    bag = Bag(_random_name())
+    store.put(bag)
+
+    for i in range(10):
+        tiddler = Tiddler(_random_name(), bag.name)
+        store.put(tiddler)
+
+    _get_entity('http://our_test_domain:8001/bags/%s/tiddlers' % bag.name)
+
+
+def _get_entity(uri):
+    """
+    Get a uri and confirm that for those relevant headers
+    in a 200 response, the same things are there for a
+    304 response.
+    """
+    response, content = http.request(uri,
+        method='GET')
+
+    assert response['status'] == '200', content
+    response_200 = response
+    etag = response['etag']
+
+    response, content = http.request(uri,
+        headers={'If-None-Match': etag},
+        method='GET')
+
+    assert response['status'] == '304', content
+    response_304 = response
+
+    for header in RELEVANT_HEADERS:
+        if header in response_200:
+            assert header in response_304
+            assert response_200[header] == response_304[header]
