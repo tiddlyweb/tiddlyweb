a89db62dabae54d455adcfa7cbfdbbbfb3901eef
Chris Dent <chris.dent@gmail.com>
2013-05-28 14:22:28 +0100

    Trim the fat off the tiddlywebconfig.py
    Yes, we do want this file to actually exist because it is
    
    a) useful for doing in situ experimentation and testing
    b) it is required by one of the tests

--- tiddlywebconfig.py
+++ tiddlywebconfig.py
@@ -1,18 +1,4 @@
 config = {
         'log_level':'DEBUG',
-        'log_syslog': 'local7',
         'extra': { 'stuff': ['one', 'two'], }, # required for test_merge_config
-        #'server_prefix': '/wiki',
-        #'system_plugins': ['tiddlywebplugins.whoosher'],
-        #'twanager_plugins': ['tiddlywebplugins.whoosher'],
-        #'indexer': 'tiddlywebplugins.whoosher',
-        #'system_plugins': ['tiddlywebwiki'],
-        #'twanager_plugins': ['whoosher'],
-        #'system_plugins': ['whoosher'],
-        #'base_tiddlywiki': 'smancypants',
-        #'server_store': ['sql3', {'db_config': 'sqlite:///test.db'}],
-        #'server_store': ['devtext', {'store_root': 'store'}],
-        #'server_store': ['ramstore', {}],
-        #'server_store': ['mappingsql', {'db_config': 'sqlite:///test.db'}],
-        #'mappingsql.table': 'tiddlers',
-        }
+}
