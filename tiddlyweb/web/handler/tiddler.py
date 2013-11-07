5517a566c0beefbbe8b50082406b6d9a84a7ef2b
FND <fnd@localhost.localdomain>
2013-11-07 21:40:20 +0100

    fixed use of quotes for consistency

--- tiddlyweb/web/handler/tiddler.py
+++ tiddlyweb/web/handler/tiddler.py
@@ -404,7 +404,7 @@ def _send_tiddler(environ, start_response, tiddler):
         response.append(last_modified)
     if etag:
         response.append(etag)
-    start_response("200 OK", response)
+    start_response('200 OK', response)
 
     if isinstance(content, basestring):
         return [content]
